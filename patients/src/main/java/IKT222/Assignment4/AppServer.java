621cbfb1133a79f67633248cd4d958e7c94180d7
Simon Nodeland <simonsn@uia.no>
2025-10-23 22:17:10 +0200

    refactor AppServer to use WebAppContext for servlet handling

--- patients/src/main/java/IKT222/Assignment4/AppServer.java
+++ patients/src/main/java/IKT222/Assignment4/AppServer.java
@@ -1,16 +1,22 @@
-package IKT222.Assignment4;
+﻿package IKT222.Assignment4;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class AppServer {
   public static void main(String[] args) throws Exception {
-    ServletHandler handler = new ServletHandler();
-    handler.addServletWithMapping(AppServlet.class, "/*");
-
     Server server = new Server(8080);
-    server.setHandler(handler);
 
+    WebAppContext webapp = new WebAppContext();
+    webapp.setContextPath("/");
+    // Use absolute path so it works no matter where gradle starts from
+    webapp.setResourceBase(new java.io.File("src/main/webapp").getAbsolutePath());
+    webapp.setParentLoaderPriority(true);
+
+    // Register our servlet (annotation mapping is also fine; this is explicit)
+    webapp.addServlet(AppServlet.class, "/*");
+
+    server.setHandler(webapp);
     server.start();
     server.join();
   }
